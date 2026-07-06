fa8d755f275e9a827339cb98ce3939ea7162fcdf
Anh-Duy Pham <phamanhduyqb@gmail.com>
2026-07-07 01:07:25 +0700

    feat(chat): improve chatbox layout and premium aesthetics

--- Front-end-AIStudyHub/src/main.tsx
+++ Front-end-AIStudyHub/src/main.tsx
@@ -1,13 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import './styles/redesign.css'
-import App from './App.tsx'
-import { ToastProvider } from './hooks/useToast.tsx'
-import { ThemeProvider } from './theme.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import "./styles/redesign.css";
+import App from "./App.tsx";
+import { ToastProvider } from "./hooks/useToast.tsx";
+import { ThemeProvider } from "./theme.tsx";
+import { Agentation } from "agentation";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <ThemeProvider>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
           <App />
         </ToastProvider>
       </ThemeProvider>
+      <Agentation />
     </BrowserRouter>
   </StrictMode>,
-)
+);
